6d462f4330a514652f4d5db9bb9ca625209a614a
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-12-14 12:56:37 +0200

    create the game button

--- secret_santa/app/management/commands/bot.py
+++ secret_santa/app/management/commands/bot.py
@@ -1,7 +1,7 @@
 import os
 from django.core.management.base import BaseCommand
-from telegram.ext import CommandHandler, CallbackContext, Updater
-from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext, Updater, CallbackQueryHandler
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 import logging
 from dotenv import load_dotenv
 
@@ -16,16 +16,28 @@ class Command(BaseCommand):
             format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
             level=logging.INFO)
 
-        bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+        bot_token = os.environ['TG_TOKEN']
 
         def start(update: Update, context: CallbackContext):
-            update.message.reply_text('Hello! I am your bot.')
+            keyboard = [
+                [InlineKeyboardButton("Создать игру", callback_data='Создать игру')],
+
+            ]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            update.message.reply_text('Организуй тайный обмен подарками, запусти праздничное настроение!',
+                                      reply_markup=reply_markup)
+
+        def button(update, _):
+            query = update.callback_query
+            query.answer()
+            query.edit_message_text(text="Создать игру")
 
         updater = Updater(bot_token, use_context=True)
         dispatcher = updater.dispatcher
+        dispatcher.add_handler(CommandHandler("start", start))
+        dispatcher.add_handler(CallbackQueryHandler(button))
 
-        dispatcher.add_handler(CommandHandler('start', start))
 
         updater.start_polling()
-
         updater.idle()
+
