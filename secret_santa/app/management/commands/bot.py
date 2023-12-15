dfb43437c543d03c4866d515e40280d4814c39ec
Nikita <barskii.no@outlook.com>
2023-12-15 16:42:10 +0300

    Expand response_data to bot, add dialog with organizator

--- secret_santa/app/management/commands/bot.py
+++ secret_santa/app/management/commands/bot.py
@@ -1,15 +1,13 @@
 import os
-from django.core.management.base import BaseCommand
-from telegram.ext import (Updater, CommandHandler, MessageHandler, 
-                          Filters, ConversationHandler, CallbackQueryHandler)
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import requests
-
 import logging
+from django.core.management.base import BaseCommand
+from telegram.ext import (Updater, CommandHandler, MessageHandler,
+                          Filters, ConversationHandler, CallbackQueryHandler)
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 class Command(BaseCommand):
     help = 'Starts the Telegram bot'
@@ -31,38 +29,81 @@ class Command(BaseCommand):
             return 'GET_GAME_NAME'
 
         def get_game_name(update, context):
+            context.chat_data['game_info'] = {}
             try:
                 context.user_data['chat_id'] = update.effective_chat.id
                 if update.message:
                     context.user_data['message_id'] = update.message.message_id
+                    context.chat_data['game_info']['owner'] = update.message.from_user['id']
                     update.message.reply_text('Введите название игры:')
-                    return 'CREATE_GAME'
+                    return 'GET_BUDGET'
                 elif update.callback_query:
                     context.user_data['message_id'] = update.callback_query.message.message_id
+                    context.chat_data['game_info']['owner'] = update.callback_query.from_user['id']
                     context.bot.send_message(chat_id=update.effective_chat.id, text='Введите название игры:')
-                    return 'CREATE_GAME'
+                    return 'GET_BUDGET'
                 else:
                     raise ValueError('No message or callback query in update')
             except Exception as e:
                 print(f"Error in get_game_name function: {e}")
-                return ConversationHandler.END
+                return 'ERROR'
+
+        def get_budget(update, context):
+            context.chat_data['game_info']['name'] = update.message.text
+            keyboard = [
+                ['Да', 'Нет']
+            ]
+            reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+            update.message.reply_text('Бюджет ограничен?', reply_markup=reply_markup)
+            return 'CHOOSE_BUDGET'
+            
+        def choose_budget(update, context):
+            if update.message.text == "Да":
+                context.chat_data['game_info']['is_limited'] = True
+                keyboard = [
+                    ['до 500', '500-1000', '1000-2000']
+                ]
+                reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+                update.message.reply_text('Выберите бюджет:', reply_markup=reply_markup)         
+                return 'REGISTRATION_DATE'
+            context.chat_data['game_info']['is_limited'] = False
+            context.chat_data['game_info']['budget'] = ''            
+            update.message.reply_text('Введите дату завершения регистрации (в формате ММ/ДД/ГГГГ):')         
+            return 'CREATE_GAME'
+
+
+        def get_registration_date(update, context):
+            context.chat_data['game_info']['budget'] = update.message.text
+            update.message.reply_text('Введите дату завершения регистрации (в формате ММ/ДД/ГГГГ):')
+            return 'CREATE_GAME'
+        
+        def print_error_message(update, context):
+            update.message.reply_text('Что-то пошло не так. Попробуйте ещё раз.')
 
         def create_game(update, context):
+            context.chat_data['game_info']['draw_date'] = update.message.text
+            game_info = context.chat_data['game_info']
             try:
-                game_name = update.message.text
-
                 django_view_url = f'http://127.0.0.1:8000/create_game/'
+                print(game_info)
+                response = requests.post(django_view_url, json={
+                    'owner': game_info['owner'],
+                    'name': game_info['name'],
+                    'is_limited': game_info['is_limited'],
+                    'budget': game_info['budget'],
+                    'draw_date': game_info['draw_date']
+                    }
+                )
 
-                response = requests.post(django_view_url, json={'game_name': game_name})
-
-                print(f"Sent POST request to Django for creating game: {game_name}")
+                print(f"Sent POST request to Django for creating game: {game_info}")
                 print(f"Response from Django: {response.text}")
 
                 data_from_db = response.json()
+                print('----------')
                 print(data_from_db)
+                print('----------')
 
-                context.bot.send_message(chat_id=context.user_data['chat_id'],
-                                         text=f'Игра "{game_name}" создана!')
+                update.message.reply_text(f'Игра {data_from_db["name"]} создана! Ссылка на игру: https://t.me/sssssssssannnttaaaa_bot/start={data_from_db["id"]}')
                 return ConversationHandler.END
             except Exception as e:
                 print(f"Error in create_game function: {e}")
@@ -75,11 +116,14 @@ class Command(BaseCommand):
             entry_points=[CommandHandler("start", start)],
             states={
                 'GET_GAME_NAME': [CallbackQueryHandler(get_game_name)],
+                'GET_BUDGET': [MessageHandler(Filters.text & ~Filters.command, get_budget)],
+                'CHOOSE_BUDGET': [MessageHandler(Filters.text & ~Filters.command, choose_budget)],
+                'REGISTRATION_DATE': [MessageHandler(Filters.text & ~Filters.command, get_registration_date)],
                 'CREATE_GAME': [MessageHandler(Filters.text & ~Filters.command, create_game)],
+                'ERROR': [MessageHandler(Filters.text & ~Filters.command, print_error_message)],
             },
             fallbacks=[],
         )
-
         dispatcher.add_handler(conv_handler)
 
         updater.start_polling()
--- secret_santa/app/views.py
+++ secret_santa/app/views.py
@@ -1,6 +1,7 @@
 from .models import Game
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
 import json
 
 @csrf_exempt
@@ -8,11 +9,24 @@ def create_game(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body.decode('utf-8'))
-            game_name = data.get('game_name')
+            #date = datetime.strptime(data.get('draw_date'), '%m/%d/%Y')
 
-            Game.objects.create(name=game_name)
-
-            response_data = {'status': 'success', 'message': f'Игра "{game_name}" создана!'}
+            Game.objects.create(
+                name=data.get('name'),
+                is_limited=data.get('is_limited'),
+                budget=data.get('budget'),
+                #draw_date=datetime(date.year,date.month,date.day,tzinfo=None),#datetime.strptime(data.get('draw_date'), '%m/%d/%Y'),
+                
+            )
+            created_game = Game.objects.last()
+            response_data = {
+                'status': 'success',
+                'id': created_game.id,
+                'name': created_game.name,
+                'is_limited': created_game.is_limited,
+                'budget': created_game.budget,
+                'draw_date': created_game.draw_date
+            }
             return JsonResponse(response_data)
         except json.decoder.JSONDecodeError:
             return JsonResponse({'status': 'error', 'message': 'Invalid JSON format in the request'})
