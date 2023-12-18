f5ced60be18151c04b5c0f67d6c9d33213e165da
Nikita <barskii.no@outlook.com>
2023-12-18 16:49:17 +0300

    Add sending draw results to users tg

--- secret_santa/app/management/commands/scheduler.py
+++ secret_santa/app/management/commands/scheduler.py
@@ -6,10 +6,9 @@ import os
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.core.management.base import BaseCommand
 from django.utils import timezone
-
 from app.models import DrawingResult, Game, Player
 from dotenv import load_dotenv
-
+from random import shuffle
 
 load_dotenv()
 logging.basicConfig(
@@ -21,12 +20,21 @@ logging.basicConfig(
 )
 
 
-def send_message(sender, recipient):
+def send_message(sender, recipient, success):
     logger = logging.getLogger(__name__)
     logger.info('sending message from {} to {}'.format(sender.tg_id, recipient.tg_id))
     bot_token = os.environ['TELEGRAM_BOT_TOKEN']
     url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
-    text = f'Розыгрыш проведен!\nВы дарите подарок {recipient.first_name} {recipient.last_name}!\nЕго пожелания - {recipient.wishes}.\n Дата отправки подарка до {recipient.game.send_date}'
+    send_date = recipient.game.send_date.astimezone(pytz.timezone('Europe/Moscow')).strftime('%d-%m-%Y %H:%M')
+    if success:
+        text = f'''Розыгрыш проведен!\n
+Вы дарите подарок 🎁 пользователю {recipient.first_name} {recipient.last_name}!
+Вот пожелания пользователя - {recipient.wishes}.
+Дата отправки подарка до {send_date}
+'''
+
+    else:
+        text = f'Розыгрыш не был проведен, слишком мало участников! Подарите подарок себе 🎁'
     data = {
         'chat_id': sender.tg_id,
         'text': text
@@ -34,23 +42,26 @@ def send_message(sender, recipient):
     response = requests.post(url, data=data)
     return response.json()
 
+
 def perform_lottery():
-    print('Performing lottery...')
-    games_to_draw = Game.objects.filter(draw_date__lte=timezone.now())
+    games_to_draw = Game.objects.filter(draw_date__lte=timezone.now(), draw_status=False)
     for game in games_to_draw:
-        print('game id = {}'.format(game.id))        
-        if game.draw_status is False:
-            players = list(Player.objects.filter(game=game))
+        players = list(Player.objects.filter(game=game))
+        if len(players) < 2:
+            send_message(players[0], players[0], False)
+        else:
+            shuffle(players)
+            print(players)
             for i in range(len(players)):
                 sender = players[i]
                 recipient = players[(i + 1) % len(players)]
                 DrawingResult.objects.create(
                     game=game, sender=sender, recipient=recipient)
                 logger = logging.getLogger(__name__)
-                response = send_message(sender, recipient)
+                response = send_message(sender, recipient, True)
                 logger.info(response)
-            game.draw_status = True
-            game.save()
+        game.draw_status = True
+        game.save()
 
 
 def start_task():
